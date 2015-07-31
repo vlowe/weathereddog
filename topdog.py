f6d897175b70a12b2f849ee3cadc3f4fc1484f47
Vicki Lowe <vicki.lowe@gmail.com>
2015-07-31 11:28:21 +1000

    Version 0.7 of weathereddog. Added conditions and cities selected randomly from a list.

--- topdog.py
+++ topdog.py
@@ -6,9 +6,17 @@ import random
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html; charset = utf-8'
+        city = random.choice(['Sydney', 'Melbourne', 'Brisbane', 'Perth',
+                              'Adelaide', 'Gold Coast', 'Canberra',
+                              'Newcastle', 'Wollongong', 'Logan City'])
         temp = random.randrange(5, 40)
+        # http://wiki.wunderground.com/index.php/Educational_-_Partly_cloudy
+        condition = random.choice(['Cloudy', 'Clear', 'Mostly Cloudy', 
+                                      'Partly Cloudy', 'Partly Sunny', 
+                                      'Mostly Sunny', 'Sunny'])
+
         self.response.write(
-            '<h1>Sydney\'s weather ' + str(temp) + '°C</h1>'
+            '<h1>' + city + '\'s weather is ' + condition + ' and ' + str(temp) + '°C.</h1>'
             '<img src="img/sleepy-pup.jpg">'
             '''<a href="https://www.flickr.com/photos/hand-nor-glove/378065479/">
                  hand-nor-glove CC BY-NC-ND 2.0
