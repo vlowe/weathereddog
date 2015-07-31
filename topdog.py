ca3a5a4efd23c501381c4db90f94ec84cef7be86
Vicki Lowe <vicki.lowe@gmail.com>
2015-07-31 10:03:59 +1000

    Version 0.6 of weathereddog. Added temperature selected with random number generator.

--- topdog.py
+++ topdog.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import webapp2
+import random
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html; charset = utf-8'
+        temp = random.randrange(5, 40)
         self.response.write(
-            '''<h1>Sydney\'s weather 15°C</h1>
-               <img src="img/sleepy-pup.jpg">
-               <a href="https://www.flickr.com/photos/hand-nor-glove/378065479/">
+            '<h1>Sydney\'s weather ' + str(temp) + '°C</h1>'
+            '<img src="img/sleepy-pup.jpg">'
+            '''<a href="https://www.flickr.com/photos/hand-nor-glove/378065479/">
                  hand-nor-glove CC BY-NC-ND 2.0
                </a>''')
 
