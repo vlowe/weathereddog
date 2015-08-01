04dd19be086e136814f71bcb9dde4e9bbd382ccb
Vicki Lowe <vicki.lowe@gmail.com>
2015-08-01 15:19:45 +1000

    Implement form where user inputs desired city.

--- topdog.py
+++ topdog.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import cgi
 from google.appengine.api import urlfetch
 import jinja2
 import json
@@ -13,39 +14,57 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions = ['jinja2.ext.autoescape'],
     autoescape = True)
 
+
+PUPPIES = [{'img': "img/sleepy-pup.jpg", 
+            'url': "https://www.flickr.com/photos/hand-nor-glove/378065479/", 
+            'licence': "hand-nor-glove CC BY-NC_ND 2.0"}, 
+           {'img': "img/rain-pup.jpg",
+            'url': "https://www.flickr.com/photos/alleykitten/3250509977/",
+            'licence': "alleykitten CC BY-NC-ND 2.0"},
+           {'img': "img/snow-pup.jpg",
+            'url': "https://www.flickr.com/photos/aukirk/16623977151/",
+            'licence': "aukirk CC BY 2.0"}]
+
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html; charset = utf-8'
-
-        url = "http://api.openweathermap.org/data/2.5/weather?lat=-33.8650&lon=151.2094"
-        fetch_weather = urlfetch.fetch(url)
-        
-        if fetch_weather.status_code == 200:
-            weather_dict = json.loads(fetch_weather.content)
-            city = weather_dict["name"]
-            temp = weather_dict["main"]["temp"] - 273.15
-            condition = weather_dict["weather"][0]["main"]
-        
-        puppy = [{'img': "img/sleepy-pup.jpg", 
-                  'url': "https://www.flickr.com/photos/hand-nor-glove/378065479/", 
-                  'licence': "hand-nor-glove CC BY-NC_ND 2.0"}, 
-                 {'img': "img/rain-pup.jpg",
-                  'url': "https://www.flickr.com/photos/alleykitten/3250509977/",
-                  'licence': "alleykitten CC BY-NC-ND 2.0"},
-                 {'img': "img/snow-pup.jpg",
-                  'url': "https://www.flickr.com/photos/aukirk/16623977151/",
-                  'licence': "aukirk CC BY 2.0"}]
-
-        template_values = {
-            'city': city,
-            'temp': temp,
-            'condition': condition,
-            'puppy': random.choice(puppy)
-        }
+        url = "http://api.openweathermap.org/data/2.5/weather?q=Sydney"
+        template_values = get_weather(url)
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
+
+    def post(self):
+        # remove white space in input city
+        city = cgi.escape(self.request.get('place').replace(" ", ""))
+
+        url = "http://api.openweathermap.org/data/2.5/weather?q=" + city
+        template_values = get_weather(url)
+        fetch_weather = urlfetch.fetch(url)
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
+def get_weather(url):
+        fetch_weather = urlfetch.fetch(url)
+
+        if fetch_weather.status_code == 200:
+            weather_dict = json.loads(fetch_weather.content)
+            city = weather_dict["name"]
+            country = weather_dict["sys"]["country"]
+            temp = weather_dict["main"]["temp"] - 273.15
+            condition = weather_dict["weather"][0]["main"]
+
+        template_values = {
+            'city': city,
+            'country': country,
+            'temp': temp,
+            'condition': condition,
+            'puppy': random.choice(PUPPIES)
+        }
+
+        return template_values
 
 
 app = webapp2. WSGIApplication([
