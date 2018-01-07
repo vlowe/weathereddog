ef3be772426a69fe3025305131ff31d937e325b5
Vicki Lowe <vlowe@umbreon.lan>
2018-01-07 15:07:49 +1100

    Use auto-formatter in VSCode

--- topdog.py
+++ topdog.py
@@ -10,15 +10,15 @@ import webapp2
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions = ['jinja2.ext.autoescape'],
-    autoescape = True)
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
-PUPPIES = [{'img': "img/sleepy-pup.jpg", 
-            'url': "https://www.flickr.com/photos/hand-nor-glove/378065479/", 
+PUPPIES = [{'img': "img/sleepy-pup.jpg",
+            'url': "https://www.flickr.com/photos/hand-nor-glove/378065479/",
             'licence': "hand-nor-glove CC BY-NC_ND 2.0",
-            'weather': "sleep"}, 
+            'weather': "sleep"},
            {'img': "img/rain-pup.jpg",
             'url': "https://www.flickr.com/photos/alleykitten/3250509977/",
             'licence': "alleykitten CC BY-NC-ND 2.0",
@@ -27,20 +27,21 @@ PUPPIES = [{'img': "img/sleepy-pup.jpg",
             'url': "https://www.flickr.com/photos/aukirk/16623977151/",
             'licence': "aukirk CC BY 2.0",
             'weather': "snow"},
-            {'img': "img/mist-pup.jpg",
+           {'img': "img/mist-pup.jpg",
             'url': "https://www.flickr.com/photos/sonstroem/38762487242/",
             'licence': "sonstroem CC BY 2.0",
-            'weather' : "mist"},
-            {'img': "img/clear-pup.jpg",
+            'weather': "mist"},
+           {'img': "img/clear-pup.jpg",
             'url': "https://www.flickr.com/photos/31867959@N04/15130078190/",
-            'licence' : "Dallas Krentzel CC By 2.0",
-            'weather' : "clear"}]
+            'licence': "Dallas Krentzel CC By 2.0",
+            'weather': "clear"}]
+
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         url = "http://api.openweathermap.org/data/2.5/weather?q=Sydney" + \
-                "&APPID=cf62fd8aa01dd3ebeada9cdec7ff6f8a"
+            "&APPID=cf62fd8aa01dd3ebeada9cdec7ff6f8a"
         template_values = get_weather(url)
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -48,10 +49,10 @@ class MainPage(webapp2.RequestHandler):
 
     def post(self):
         # remove white space in input city
-        field = {"q" : cgi.escape(self.request.get('place').replace(" ", ""))}
+        field = {"q": cgi.escape(self.request.get('place').replace(" ", ""))}
         city = urllib.urlencode(field)
         url = "http://api.openweathermap.org/data/2.5/weather?" + city + \
-                "&APPID=cf62fd8aa01dd3ebeada9cdec7ff6f8a"
+            "&APPID=cf62fd8aa01dd3ebeada9cdec7ff6f8a"
 
         template_values = get_weather(url)
         fetch_weather = urlfetch.fetch(url)
@@ -61,32 +62,32 @@ class MainPage(webapp2.RequestHandler):
 
 
 def get_weather(url):
-        fetch_weather = urlfetch.fetch(url)
+    fetch_weather = urlfetch.fetch(url)
 
-        if fetch_weather.status_code == 200:
-            weather_dict = json.loads(fetch_weather.content)
-            city = weather_dict["name"]
-            country = weather_dict["sys"]["country"]
-            temp = round(weather_dict["main"]["temp"] - 273.15, 1)
-            condition = weather_dict["weather"][0]["main"]
+    if fetch_weather.status_code == 200:
+        weather_dict = json.loads(fetch_weather.content)
+        city = weather_dict["name"]
+        country = weather_dict["sys"]["country"]
+        temp = round(weather_dict["main"]["temp"] - 273.15, 1)
+        condition = weather_dict["weather"][0]["main"]
 
-        displayed_puppy = PUPPIES[0]
+    displayed_puppy = PUPPIES[0]
 
-        for puppy in PUPPIES:
-            if puppy['weather'] == condition.lower():
-                displayed_puppy = puppy
+    for puppy in PUPPIES:
+        if puppy['weather'] == condition.lower():
+            displayed_puppy = puppy
 
-        template_values = {
-            'city': city,
-            'country': country,
-            'temp': temp,
-            'condition': condition,
-            'puppy': displayed_puppy
-        }
+    template_values = {
+        'city': city,
+        'country': country,
+        'temp': temp,
+        'condition': condition,
+        'puppy': displayed_puppy
+    }
 
-        return template_values
+    return template_values
 
 
 app = webapp2. WSGIApplication([
     ('/', MainPage),
-], debug = True)
+], debug=True)
